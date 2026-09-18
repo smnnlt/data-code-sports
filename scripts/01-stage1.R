77daf96a0acc494323f791908dd3ff4284d5b8ff
Simon Nolte <81624911+smnnlt@users.noreply.github.com>
2026-09-18 08:28:03 +0200

    update stage 1 script

--- scripts/01-stage1.R
+++ scripts/01-stage1.R
@@ -6,22 +6,16 @@
 # load packages
 library(readr)
 library(binom)
-
-# expected sample size
-i <- 10 # number of years
-k <- 25 # number of journals 
-n_per_ik <- 175 # number of articles per journal per year
-drop <- 0.2 # assumed exclusion rate
-N <- i * k * n_per_ik * (1-drop) # expected total sample size
-N
-#> 35000
+library(purrr)
+library(tidyr)
 
 # expected time for manual download
+N <- 55000
 pa <- 0.25 # proportion of automatically retrieved studies
 t <- 16.5 # time per manual retrieval (in s)
 nr <- N * (1 - pa) # studies that need manual retrieval
 (nr * t) / (60 * 60)
-# 120 hours
+# 189 hours
 
 # data sharing in Borg et al. (2020) for only original articles
 # read data provided on GitHub 
@@ -46,3 +40,49 @@ table(emp$Data_shared) |> prop.table()
 # number of articles sharing code
 table(emp$Code_shared)
 #> 0
+
+# journals included in the article search
+# export SCImago journal rank data from website
+
+# read data for years 2017-2026
+read_scimago <- function(year) {
+  d <- read.csv2(paste0("data/scimago/raw/", year, ".csv"))
+  data.frame(
+    issn = d$Issn,
+    title = d$Title,
+    q = d$SJR.Quartile,
+    year = year
+  )
+}
+allyears <- lapply(2017:2025, read_scimago) |> list_rbind()
+
+# quartile ranking as factor
+allyears$q <- as.numeric(factor(allyears$q, levels = c("Q1", "Q2", "Q3", "Q4", labels = 1:4)))
+# to wide format
+ranking <- pivot_wider(
+  allyears,
+  id_cols = c(issn, title),
+  names_from = year,
+  values_from = q
+)
+
+# journals that were at least three years in Q1
+sum(rowSums(ranking == 1, na.rm = TRUE) >= 3)
+#> 36
+
+# get list of journals
+jlist <- ranking$title[rowSums(ranking == 1, na.rm = TRUE) >= 3] 
+sort(jlist) |> paste(collapse = "; ")
+
+# exclude the following:
+# Exercise and Sport Sciences Reviews (only review articles)
+# Exercise Immunology Review (only review articles)
+# International Review of Sport and Exercise Psychology (only review articles)
+# Qualitative Research in Sport, Exercise and Health (mainly qualitative work)
+# Sport, Education and Society (mainly qualitative work)
+
+# precision estimation for error rates (verification set)
+# assuming an error rate of 1%
+err <- binom.confint(0.01*500, 500, methods = "ac")
+err$upper - err$lower # 95% confidence interval width
+#> 0.0203
